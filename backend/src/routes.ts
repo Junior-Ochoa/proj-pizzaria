9d7429a731d8d2a5133e7d6fa0214648e11507bf
Junior Ochoa <ademirks@gmail.com>
2023-08-10 16:52:32 -0300

    Rota produtos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,18 +7,24 @@ import { DetailUserController } from './controllers/user/DetailUserController'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 
+import { CreateProductController } from './controllers/product/CreateProductController'
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router();
 
-//-- ROTAS USER -- 
+//-- ROTAS USER 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
-//-- ROTAS CATEGORY --
+//-- ROTAS CATEGORY 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle  )
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, new CreateProductController().handle )
+
 
 
 
