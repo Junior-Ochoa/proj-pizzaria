3dce76b7834a055a72931f14f44159cc3f5615e0
Junior Ochoa <ademirks@gmail.com>
2023-08-18 14:44:24 -0300

    Fechando um pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,19 +1,21 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import { CreateUserController } from './controllers/user/CreateUserController'
-import { AuthUserController } from './controllers/user/AuthUserController'
-import { DetailUserController } from './controllers/user/DetailUserController'
+import {CreateUserController} from './controllers/user/CreateUserController'
+import {AuthUserController} from './controllers/user/AuthUserController'
+import {DetailUserController} from './controllers/user/DetailUserController'
 
-import { CreateCategoryController } from './controllers/category/CreateCategoryController'
-import { ListCategoryController } from './controllers/category/ListCategoryController'
+import {CreateCategoryController} from './controllers/category/CreateCategoryController'
+import {ListCategoryController} from './controllers/category/ListCategoryController'
 
-import { CreateProductController } from './controllers/product/CreateProductController'
-import { ListByCategoryController } from './controllers/product/ListByCategoryController'
+import {CreateProductController} from './controllers/product/CreateProductController'
+import {ListByCategoryController} from './controllers/product/ListByCategoryController'
 
-import { CreateOrderController } from './controllers/order/CreateOrderController'
+import {CreateOrderController} from './controllers/order/CreateOrderController'
+import {RemoveOrderController} from './controllers/order/RemoveOrderController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
+
 
 import uploadConfig from './config/multer'
 
@@ -35,6 +37,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 //- ROTAS ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 
 
