0d33897b2ae87fd4089b9b36f7bfc1ed0c6876ed
Junior Ochoa <ademirks@gmail.com>
2023-08-28 17:10:56 -0300

    Criando contexto

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,32 @@
+import {useContext, FormEvent} from 'react'
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss';
 
 import {Input} from '../components/ui/Input'
 import {Button} from '../components/ui/Button'
+import Link from 'next/link';
+import {AuthContext} from '../contexts/AuthContext';
 
 import logoImg from '../../public/logo.svg';
 
-import Link from 'next/link';
 
 export default function Home() {
+  const {signIn} = useContext(AuthContext)
+  
+  async function handleLogin(event: FormEvent ){
+    event.preventDefault();
+
+    let data = {
+      email: "teste@teste.com",
+      password: "123123"
+    }
+
+    await signIn(data)
+
+  }
+
+
   return (
       <>
        <Head>
@@ -19,7 +36,7 @@ export default function Home() {
           <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
 
           <div className={styles.login}>
-            <form>
+            <form onSubmit={handleLogin}>
                 <Input
                 placeholder='Digite seu email'
                 type="text"
