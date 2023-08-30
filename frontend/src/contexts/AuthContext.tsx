6e9d0067a2aaa55b78345c0b580245091a8c6989
Junior Ochoa <ademirks@gmail.com>
2023-08-30 16:25:48 -0300

    Fazendo login

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import {createContext, ReactNode, useState} from 'react';
-import {destroyCookie} from 'nookies'
+import {destroyCookie, setCookie, parseCookies} from 'nookies'
 import Router from 'next/router'
+import {api} from '../services/apiClient'
 
 type AuthContextData = {
     user: UserProps;
@@ -41,8 +42,34 @@ export function AuthProvider({children}: AuthProviderProps){
     const isAuthenticated = !!user;
 
     async function signIn({email, password}: SignInProps){
-       console.log("DADOS PARA LOGAR ", email)
-       console.log("SENHA ", password)
+       try{
+        const response = await api.post('/session', {
+            email,
+            password
+        })
+        // console.log(response.data);
+        const {id, name, token} = response.data;
+
+        setCookie(undefined, '@nextauth.token', token,{
+            maxAge: 60 * 60 * 24 * 30, //expira em 1 mes
+            path: "/" //quais caminhos terao acesso ao cookie
+        })
+
+        setUser({
+           id,
+           name,
+           email, 
+        })
+        //passar para proximas requisiçoes nosso token
+        api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+        //redirecionar o user para /dashboard
+        Router.push('/dashboard')
+
+       
+        }catch(err){
+            console.log("ERRO AO ACESSAR ", err)
+       }
     }
 
 
