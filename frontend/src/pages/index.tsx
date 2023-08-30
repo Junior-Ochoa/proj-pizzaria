6fc63397a93bed413dd384384b952758e02056a4
Junior Ochoa <ademirks@gmail.com>
2023-08-30 14:19:31 -0300

    Enviando dados login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {useContext, FormEvent} from 'react'
+import {useContext, FormEvent, useState} from 'react'
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss';
@@ -13,13 +13,18 @@ import logoImg from '../../public/logo.svg';
 
 export default function Home() {
   const {signIn} = useContext(AuthContext)
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  
   
   async function handleLogin(event: FormEvent ){
     event.preventDefault();
 
     let data = {
-      email: "teste@teste.com",
-      password: "123123"
+      email,
+      password
     }
 
     await signIn(data)
@@ -40,11 +45,15 @@ export default function Home() {
                 <Input
                 placeholder='Digite seu email'
                 type="text"
+                value={email}
+                onChange={ (e) => setEmail(e.target.value) }
                 />
 
                 <Input
                 placeholder='Digite sua senha'
                 type="password"
+                value={password}
+                onChange={ (e) => setPassword(e.target.value) }
                 />
 
                 <Button
