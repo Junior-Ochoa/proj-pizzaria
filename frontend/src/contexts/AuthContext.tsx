d701b9f201bb12c5a8c70397f5a2239e76cab374
Junior Ochoa <ademirks@gmail.com>
2023-09-26 15:22:19 -0300

    Permanecendo login

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-import {createContext, ReactNode, useState} from 'react';
-import {destroyCookie, setCookie, parseCookies} from 'nookies'
+import { createContext, ReactNode, useState, useEffect } from 'react'
+import { destroyCookie, setCookie, parseCookies } from 'nookies'
 import Router from 'next/router'
-import {api} from '../services/apiClient'
+import { api } from '../services/apiClient'
 import { toast } from 'react-toastify'
 
 type AuthContextData = {
@@ -48,6 +48,28 @@ export function signOut(){
 export function AuthProvider({children}: AuthProviderProps){
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        
+        const { '@nextauth.token': token } = parseCookies()
+
+        if(token){
+            api.get('/me').then(response =>{
+                const { id, name, email} = response.data;
+
+                setUser({
+                   id,
+                   name,
+                   email
+                })
+            })
+            .catch(() =>{
+                //se deu erro deslogamos o user
+                signOut();
+            })
+        }
+
+    }, [])
 
     async function signIn({email, password}: SignInProps){
        try{
